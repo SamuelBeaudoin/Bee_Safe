08a1b116fd4a19875f365fb99c840b36e58ecf04
Theodor Semerdzhiev <tedi.semerdjiev@hotmail.com>
2024-01-20 13:07:34 -0500

    zxczxc
    zxczx

---
+++ test1.py
@@ -0,0 +1,12 @@
+import plotly.figure_factory as ff
+import plotly.express as px
+
+px.set_mapbox_access_token(open(".mapbox_token").read())
+df = px.data.carshare()
+
+fig = ff.create_hexbin_mapbox(
+    data_frame=df, lat="centroid_lat", lon="centroid_lon",
+    nx_hexagon=10, opacity=0.5, labels={"color": "Point Count"},
+    min_count=1,
+)
+fig.show()
