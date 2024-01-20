8be2b128c7f233c50e5c231cfe20a0bb534f4364
Arthur Acker <arthuracker07@gmail.com>
2024-01-20 14:11:42 -0500

    merged data

--- src/main.py
+++ src/main.py
@@ -1,14 +1,16 @@
 import plotly.figure_factory as ff
 import plotly.express as px
+import pandas as pd
+import data_merging
 
 px.set_mapbox_access_token(open("src/.mapbox_token").read())
-df = px.data.carshare()
+df = data_merging.merge_data()
 
 fig = ff.create_hexbin_mapbox(
-    data_frame=df, lat="centroid_lat", lon="centroid_lon",
-    nx_hexagon=25, opacity=0.5, labels={"color": "Point Count"},
+    data_frame=df, lat="Latitude", lon="Longitude",
+    nx_hexagon=150, opacity=0.5, labels={"color": "Point Count"},
     min_count=1, color_continuous_scale="Viridis",
-    show_original_data=True,
-    original_data_marker=dict(size=4, opacity=0.6, color="deeppink")
+    #show_original_data=True,
+    #original_data_marker=dict(size=4, opacity=0.6, color="deeppink")
 )
 fig.show()
