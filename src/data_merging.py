34c5371bf8c3a763ca8fd5decef1362aa050ffd1
Theodor Semerdzhiev <tedi.semerdjiev@hotmail.com>
2024-01-20 15:48:28 -0500

    .

--- src/data_merging.py
+++ src/data_merging.py
@@ -1,14 +1,15 @@
 import pandas as pd
+from compute_scores import preprocess_crime_data, proprocess_car_crash_data
 import os
 import glob
 
 def merge_data():
     # Get all the files in the data directory
-    df1 = pd.read_csv("data/actes-criminels.csv")
-    df2 = pd.read_csv("data/rues-pietonnes.csv")
-    df3 = pd.read_csv("data/feux-pietons.csv")
-    df4 = pd.read_csv("data/feux-circulation.csv")
-    df5 = pd.read_csv("data/collisions-routieres.csv")
+    df1 = preprocess_crime_data()
+    df2 = pd.read_csv("../data/rues-pietonnes.csv")
+    df3 = pd.read_csv("../data/feux-pietons.csv")
+    df4 = pd.read_csv("../data/feux-circulation.csv")
+    df5 = proprocess_car_crash_data()
 
     # Renaming latitude and longitude columns for consistency and adding 'Type' column
     df1 = df1.rename(columns={'LATITUDE': 'Latitude', 'LONGITUDE': 'Longitude'})
@@ -16,22 +17,25 @@ def merge_data():
 
     df2 = df2.rename(columns={'LATITUDE': 'Latitude', 'LONGITUDE': 'Longitude'})
     df2['Type'] = 'rues-pietonnes'
+    df2['COST'] = 0
 
     df3 = df3.rename(columns={'Latitude': 'Latitude', 'Longitude': 'Longitude'})
     df3['Type'] = 'feux-pietons'
+    df3['COST'] = 0
 
     df4 = df4.rename(columns={'Latitude': 'Latitude', 'Longitude': 'Longitude'})
     df4['Type'] = 'feux-circulation'
+    df4['COST'] = 0
 
     df5 = df5.rename(columns={'LOC_LAT': 'Latitude', 'LOC_LONG': 'Longitude'})
     df5['Type'] = 'collisions-routieres'
 
     # Selecting only the required columns
-    df1 = df1[['Type', 'Latitude', 'Longitude']]
-    df2 = df2[['Type', 'Latitude', 'Longitude']]
-    df3 = df3[['Type', 'Latitude', 'Longitude']]
-    df4 = df4[['Type', 'Latitude', 'Longitude']]
-    df5 = df5[['Type', 'Latitude', 'Longitude']]
+    df1 = df1[['Type', 'Latitude', 'Longitude', 'COST']]
+    df2 = df2[['Type', 'Latitude', 'Longitude', 'COST']]
+    df3 = df3[['Type', 'Latitude', 'Longitude', 'COST']]
+    df4 = df4[['Type', 'Latitude', 'Longitude', 'COST']]
+    df5 = df5[['Type', 'Latitude', 'Longitude', 'COST']]
 
     # Concatenating all dataframes
     merged_df = pd.concat([df1, df2, df3, df4, df5])
@@ -40,7 +44,7 @@ def merge_data():
     merged_df = merged_df.dropna()
 
     # Saving the merged dataframe to a new CSV file
-    output_file = 'data/merged_data.csv'
+    output_file = '../data/merged_data.csv'
     merged_df.to_csv(output_file, index=False)
 
     return merged_df
--- src/main.py
+++ src/main.py
@@ -1,7 +1,13 @@
 import plotly.figure_factory as ff
 import plotly.express as px
 import pandas as pd
+import numpy as np
 import data_merging
+
+
+# Define a custom aggregation function to calculate the average of the "cost" attribute for each row
+def custom_agg(row):
+    return np.mean(row['COST'])
 
 def main():
     px.set_mapbox_access_token(open(".mapbox_token").read())
@@ -9,11 +15,16 @@ def main():
 
     fig = ff.create_hexbin_mapbox(
         data_frame=df, lat="Latitude", lon="Longitude",
-        nx_hexagon=150, opacity=0.5, labels={"color": "Point Count"},
+        nx_hexagon=150, opacity=0.5, labels={"color": "Cost"},
         min_count=1, color_continuous_scale="Viridis",
-        #show_original_data=True,
+        color="COST", 
+        agg_func=np.mean, 
+        range_color=[0,30]
+        # show_original_data=True,
         #original_data_marker=dict(size=4, opacity=0.6, color="deeppink")
+        
     )
+
     fig.show()
 
 if __name__ == "__main__": 
