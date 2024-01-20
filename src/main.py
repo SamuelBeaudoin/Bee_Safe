3f36108c99ba8b7f7cf59a684e3e1fe94d3b4b36
Arthur Acker <arthuracker07@gmail.com>
2024-01-20 14:13:54 -0500

    main func

--- src/main.py
+++ src/main.py
@@ -3,14 +3,18 @@ import plotly.express as px
 import pandas as pd
 import data_merging
 
-px.set_mapbox_access_token(open("src/.mapbox_token").read())
-df = data_merging.merge_data()
+def main():
+    px.set_mapbox_access_token(open("src/.mapbox_token").read())
+    df = data_merging.merge_data()
 
-fig = ff.create_hexbin_mapbox(
-    data_frame=df, lat="Latitude", lon="Longitude",
-    nx_hexagon=150, opacity=0.5, labels={"color": "Point Count"},
-    min_count=1, color_continuous_scale="Viridis",
-    #show_original_data=True,
-    #original_data_marker=dict(size=4, opacity=0.6, color="deeppink")
-)
-fig.show()
+    fig = ff.create_hexbin_mapbox(
+        data_frame=df, lat="Latitude", lon="Longitude",
+        nx_hexagon=150, opacity=0.5, labels={"color": "Point Count"},
+        min_count=1, color_continuous_scale="Viridis",
+        #show_original_data=True,
+        #original_data_marker=dict(size=4, opacity=0.6, color="deeppink")
+    )
+    fig.show()
+
+if __name__ == "__main__": 
+    main()
